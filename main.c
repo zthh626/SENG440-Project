4c12f5f4a93fd2684aefea843c1c47bcabee4d7d
Alex Huang <alexhuang@uvic.ca>
2023-06-22 10:26:51 -0700

    feat: push

--- main.c
+++ main.c
@@ -1,15 +1,31 @@
-#include "arm_neon.h"
+#include <stdint.h>
 #include <stdio.h>
 
-int x = 0;
+/* Define constants to improve readability */
+#define BLOCK_SIZE 16
+#define STRIDE 2
 
-int main( void) {
-    printf("hello world");
-    x += 2;
-    printf("hello world %d", x);
-    uint32x2_t aa, bb, ss;
+int x = 1, y = 2, r = 3, s = 4;
 
-    ss = vadd_u32( aa, bb);
+int main()
+{
+   int A[16][16] = {{0}}, B[16][16] = {{0}}, diff1, diff2, sad = 0;
+   int i, j;
+   for (i = 0; i < 16; i++){
+       for (j = 0; j < 16; j += 2)
+       {
+           diff1 = A[x + i][y + j] - B[(x + r) + i][(y + s) + j];
+           diff2 = A[x + i][y + j + 1] - B[(x + r) + i][(y + s) + j + 1];
+           if (diff1 < 0)
+               sad -= diff1;
+           else
+               sad += diff1;
+           if (diff2 < 0)
+               sad -= diff2;
+           else
+               sad += diff2;
+       }
+    }
 
-    return 0;
+    printf("SAD: %d\n", sad);
 }
