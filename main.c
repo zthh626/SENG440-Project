9ced3d82234a01fd8be04b3ced64873eb75b9418
Matthew Van Brummelen <vb.matthew@gmail.com>
2023-08-02 19:56:13 -0700

    updated the comment formatting to be readable in screenshot form

--- main.c
+++ main.c
@@ -15,23 +15,31 @@
 
 #define MAX_SAD_VAL (BLOCK_SIZE * BLOCK_SIZE * MAX_DIFF)
 /*
-In the following modified code, we load 16 elements at a time into NEON registers using vld1q_s8 and subtract them using vsubq_s8.
-We then calculate the absolute difference using vabsq_s8.
-The resulting absolute differences are accumulated into the sad vector using vaddq_s8.
-Finally, we reduce the 8-bit accumulations to a single 64-bit value using pairwise additions with vpaddlq_s8, vpaddlq_s16, vpaddlq_s32, and vgetq_lane_s64.
+In the following modified code, we load 16 elements at a time into NEON registers
+using vld1q_s8 and subtract them using vsubq_s8. We then calculate the absolute
+difference using vabsq_s8. The resulting absolute differences are accumulated
+into the sad vector using vaddq_s8. Finally, we reduce the 8-bit accumulations
+to a single 64-bit value using pairwise additions with vpaddlq_s8, vpaddlq_s16,
+vpaddlq_s32, and vgetq_lane_s64.
 */
 
-// This implemenation assumes that BLOCK_SIZE is 16
+// CONSTRAINT: BLOCK_SIZE is 16
 int sad(int8_t A[BLOCK_SIZE][BLOCK_SIZE], int8_t B[BLOCK_SIZE][BLOCK_SIZE])
 {
     int8x16_t diff1, diff2, sad1 = vdupq_n_s8(0), sad2 = vdupq_n_s8(0);
     int i;
 
-    // Unrolling this loop once allows us to software pipeline; We interleaved the instructions
-    // in a way that avoids consecutive dependent instructions, allowing for parallel execution.
+    /*
+    Unrolling this loop once allows us to software pipeline;
+    We interleaved the instructions in a way that avoids consecutive
+    dependent instructions, allowing for parallel execution.
+    */
     for (i = 0; i < BLOCK_SIZE; i+=2)
     {
-        // These are contiguous memory accesses; this is efficient for the cache, prefetching, and data transfer
+        /* 
+        These are contiguous memory accesses.
+        This is efficient for the cache, prefetching, and data transfer
+        */
         int8x16_t aVec1 = vld1q_s8(&A[i][0]);
         int8x16_t aVec2 = vld1q_s8(&A[i+1][0]);
 
@@ -57,21 +65,25 @@ int sad(int8_t A[BLOCK_SIZE][BLOCK_SIZE], int8_t B[BLOCK_SIZE][BLOCK_SIZE])
 }
 
 
-// This function initializes a block as a block of an image at starting point x, y
 /*
-To improve the init_block function to use Neon intrinsics, we can take advantage of Neon's 128-bit registers and load and store multiple elements at once.
-Here's an updated version of the init_block function using Neon intrinsics:
+This function initializes a block as a block of an image at starting point x, y.
+
+To improve the init_block function, we take advantage of Neon's 128-bit registers
+by loading and storing multiple elements at once.
 
 Explanation:
 
-We use int8x16_t to represent an 16-element vector of 8-bit integers, which corresponds to a Neon register.
-The inner loop is executed BLOCK_SIZE times to load and store elements column-wise.
-The vld1q_s8 function is used to load a Neon register with 16 elements from the image array starting at the specified address (&image[x + i][y + 0]).
-The vst1q_s8 function is used to store the 16 elements from the Neon register to the block array starting at the specified address (&block[i][0]).
-By using Neon intrinsics, this updated init_block function can load and store multiple elements at once, effectively improving the memory access efficiency and potentially enhancing the performance of the code.
+We use int8x16_t to represent an 16-element vector of 8-bit integers, which corresponds
+to a Neon register. The inner loop is executed BLOCK_SIZE times to load and store elements
+column-wise. The vld1q_s8 function loads a Neon register with 16 elements from the image
+array starting at the specified address (&image[x + i][y + 0]). The vst1q_s8 function stores
+the 16 elements from the Neon register to the block array starting at the specified address (&block[i][0]).
+
+Using Neon intrinsics in init_block enables loading and storing multiple elements at once,
+improving the memory access efficiency and performance.
 */
 
-// This implementation assumes that BLOCK_SIZE is 16
+// CONSTRAINT: BLOCK_SIZE is 16
 void init_block(int x, int y, int8_t image[IMG_W][IMG_H], int8_t block[BLOCK_SIZE][BLOCK_SIZE])
 {
 
@@ -80,13 +92,16 @@ void init_block(int x, int y, int8_t image[IMG_W][IMG_H], int8_t block[BLOCK_SIZ
 
     for (int i = 0; i < BLOCK_SIZE; i++)
     {
-        // The inner loop is removed and j is replaced with 0 because we are loading a full row at a time
+        // The inner loop is removed and j is replaced with 0 because we load a whole row at a time
         int8x16_t row = vld1q_s8(&image[x + i][y + 0]);
         vst1q_s8(&block[i][0], row);
     }
 }
 
-// This function initializes an image with a rectangle of size shape_w, shape_h at starting point shape_x, shape_y
+/*
+This function initializes an image with a rectangle
+of size shape_w, shape_h at starting point shape_x, shape_y.
+*/
 void initImage(int8_t image[IMG_W][IMG_H], int shape_w, int shape_h, int shape_x, int shape_y) {
     // Assert that the shape is within the image
     assert(shape_x >= 0);
@@ -97,7 +112,6 @@ void initImage(int8_t image[IMG_W][IMG_H], int shape_w, int shape_h, int shape_x
     for(int i = 0; i < IMG_W; i++){
         for(int j = 0; j < IMG_H; j++){
 
-            // Make the shape
             if (i >= shape_x && i < (shape_x + shape_w) && j >= shape_y && j < (shape_y + shape_h)){
                 // Make the shape
                 image[i][j] = 7;
@@ -182,7 +196,8 @@ int main()
             }
 
             // Print the best motion vector match for block A
-            printf("Motion vector: (%d, %d) for BlockA starting at: (%d, %d)\n", motionVectorX, motionVectorY, x, y);
+            printf("Motion vector: (%d, %d) for BlockA starting at: (%d, %d)\n",
+            motionVectorX, motionVectorY, x, y);
 
             // Reset values
             motionVectorX = 0;
